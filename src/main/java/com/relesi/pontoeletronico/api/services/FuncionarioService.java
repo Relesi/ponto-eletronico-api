3eb8cbb53886ad7fe303de039393dac2a02246ca
Renato Lessa <renatolessa.2011@hotmail.com>
2018-03-16 00:09:05 -0300

    Criando serviço de funcionario

---
+++ src/main/java/com/relesi/pontoeletronico/api/services/FuncionarioService.java
@@ -0,0 +1,43 @@
+package com.relesi.pontoeletronico.api.services;
+
+import com.relesi.pontoeletronico.api.entities.Funcionario;
+
+import java.util.Optional;
+
+
+
+public interface FuncionarioService {
+
+    /**
+     * Persiste um funcionário na base de dados.
+     *
+     * @param funcionario
+     * @return Funcionario
+     */
+    Funcionario persistir(Funcionario funcionario);
+
+    /**
+     * Busca e retorna um funcionário dado um CPF.
+     *
+     * @param cpf
+     * @return Optional<Funcionario>
+     */
+    Optional<Funcionario> buscarPorCpf(String cpf);
+
+    /**
+     * Busca e retorna um funcionário dado um email.
+     *
+     * @param email
+     * @return Optional<Funcionario>
+     */
+    Optional<Funcionario> buscarPorEmail(String email);
+
+    /**
+     * Busca e retorna um funcionário por ID.
+     *
+     * @param id
+     * @return Optional<Funcionario>
+     */
+    Optional<Funcionario> buscarPorId(Long id);
+
+}
---
+++ src/main/java/com/relesi/pontoeletronico/api/services/impl/FuncionarioServiceImpl.java
@@ -0,0 +1,43 @@
+package com.relesi.pontoeletronico.api.services.impl;
+
+import com.relesi.pontoeletronico.api.entities.Funcionario;
+import com.relesi.pontoeletronico.api.repositories.FuncionarioRepository;
+import com.relesi.pontoeletronico.api.services.FuncionarioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+
+@Service
+public class FuncionarioServiceImpl implements FuncionarioService {
+
+    private static final Logger log = LoggerFactory.getLogger(FuncionarioServiceImpl.class);
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    public Funcionario persistir(Funcionario funcionario) {
+        log.info("Persistindo funcionário: {}", funcionario);
+        return this.funcionarioRepository.save(funcionario);
+    }
+
+    public Optional<Funcionario> buscarPorCpf(String cpf) {
+        log.info("Buscando funcionário pelo CPF {}", cpf);
+        return Optional.ofNullable(this.funcionarioRepository.findByCpf(cpf));
+    }
+
+    public Optional<Funcionario> buscarPorEmail(String email) {
+        log.info("Buscando funcionário pelo email {}", email);
+        return Optional.ofNullable(this.funcionarioRepository.findByEmail(email));
+    }
+
+    public Optional<Funcionario> buscarPorId(Long id) {
+        log.info("Buscando funcionário pelo IDl {}", id);
+        return Optional.ofNullable(this.funcionarioRepository.findOne(id));
+    }
+
+}
